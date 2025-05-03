3ccf43d8348a4c398ceeec6c251eb238ddb1b2b4
Petercarsten <phillipstitz@gmx.de>
2025-05-03 20:58:06 +0200

    Port dynamisch für Render gesetzt

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 
 // Supabase-Konfiguration (Ersetze durch deine Daten)
